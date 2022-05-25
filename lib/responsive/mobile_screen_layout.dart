0c431a8abedecfe7f02ed41647f84403379f19ff
Nataly Koval <nataly.koval@n3wnormal.com>
2022-05-25 13:23:25 +0200

    add BottomNavigationBar to the mobile screen

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/providers/user_provider.dart';
+import 'package:instagram_flutter/utils/colors.dart';
 import 'package:provider/provider.dart';
 import '../models/user.dart' as model;
+import '../utils/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -13,32 +15,75 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
-  // String username = '';
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getUsername();
-  // }
-  //
-  // void getUsername() async {
-  //   DocumentSnapshot snap = await FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(FirebaseAuth.instance.currentUser!.uid)
-  //       .get();
-  //
-  //   setState(() {
-  //     username = (snap.data() as Map<String, dynamic>)['username'];
-  //   });
-  // }
+  int _page = 0;
+
+  late PageController pageController;
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+    // setState(() {
+    //   _page = page;
+    // });
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    model.User user = Provider.of<UserProvider>(context).getUser;
-
     return Scaffold(
-      body: Center(
-        child: Text(user.username),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+Text('111111111111'),
+Text('2222222222'),
+Text('4444444444'),
+Text('66666666666'),
+Text('777777777'),
+
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: mobileBackgroundColor,
+        type: BottomNavigationBarType.fixed,
+        onTap: navigationTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: _page == 0 ? primaryColor : secondaryColor,),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, color: _page == 1 ? primaryColor : secondaryColor,),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle, color: _page == 2 ? primaryColor : secondaryColor,),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite, color: _page == 3 ? primaryColor : secondaryColor,),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: _page == 4 ? primaryColor : secondaryColor,),
+            label: '',
+          ),
+        ],
       ),
     );
   }
